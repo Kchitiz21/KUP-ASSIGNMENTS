a0d3b241ca9dc798e20bb10e36d234d8a35d62bc
Kchitiz21 <kchitiz.deep@knoldus.com>
2021-10-12 22:20:46 +0530

    Added code for doing operations and student marking structure

---
+++ Assignment-4/Operation/src/main.rs
@@ -0,0 +1,29 @@
+
+fn main() {
+    let numbers = Numbers {
+        first_number: 30,
+        second_number: 50,
+
+    };
+    struct Numbers {
+        first_number: u32,
+        second_number: u32,
+    }
+
+    impl Numbers {
+        fn add(&self) -> u32 {
+            self.first_number + self.second_number
+
+        }
+        fn subtract(&self) -> u32 {
+            if self.first_number > self.second_number {
+                self.first_number - self.second_number
+            } else {
+                self.second_number - self.first_number
+            }
+        }
+        fn multiply(&self) -> u32 {
+            self.first_number * self.second_number
+        }
+    }
+}
