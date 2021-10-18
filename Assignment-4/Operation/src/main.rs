b4cbf73435007023b751ed227e9336c1e4fe7e4d
Kchitiz21 <88480845+Kchitiz21@users.noreply.github.com>
2021-10-18 12:51:55 +0530

    Added code for doing operations and student marking structure

--- Assignment-4/Operation/src/main.rs
+++ Assignment-4/Operation/src/main.rs
@@ -1,3 +1,4 @@
+// Main function
 
 fn main() {
     let numbers = Numbers {
@@ -5,16 +6,42 @@ fn main() {
         second_number: 50,
 
     };
+    // Numbers Structure
+    
     struct Numbers {
         first_number: u32,
         second_number: u32,
     }
-
+    
+    // Implmenting Numbers Structure
+    
     impl Numbers {
+        
+        
+     //Function "add" adds numbers using structures to function
+     // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns addition of the two numbers
+        
         fn add(&self) -> u32 {
             self.first_number + self.second_number
 
         }
+        
+    // Function "subtract" substract numbers using structures to function
+    //
+    // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns subtraction of the two numbers
+        
         fn subtract(&self) -> u32 {
             if self.first_number > self.second_number {
                 self.first_number - self.second_number
@@ -22,6 +49,16 @@ fn main() {
                 self.second_number - self.first_number
             }
         }
+        
+    // Function "multiply" multiplies numbers using structures to function
+    //
+    // #Arguments
+    //
+    // Instance of the Structure Number
+    //
+    // #Return
+    //
+    // Returns product of the two numbers
         fn multiply(&self) -> u32 {
             self.first_number * self.second_number
         }
